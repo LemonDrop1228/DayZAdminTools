4957a5f5c4b378be9c27d71864c1ddbe59f7f25d
gopherskid <gopherskid@gmail.com>
2021-11-02 17:30:11 -0400

    Continued work on the spawn position editor

--- Views/SpawnPointsEditor/SpawnPointsEditorView.xaml.cs
+++ Views/SpawnPointsEditor/SpawnPointsEditorView.xaml.cs
@@ -1,14 +1,17 @@
-﻿using System.Windows.Controls;
+﻿using DayZTediratorToolz.Helpers;
+using System.Windows.Controls;
 
 namespace DayZTediratorToolz.Views
 {
     public partial class SpawnPointsEditorView : BaseView
     {
+        public const string _titleKey = "SpawnPositionTitle";
         public override ViewMenuData ViewMenuData { get; set; }
         public SpawnPointsEditorView()
         {
-            ViewMenuData = new ViewMenuData() { ViewIcon = MaterialDesignThemes.Wpf.PackIconKind.Download, ViewIndex=4,    };
+            ViewMenuData = new ViewMenuData() { ViewIcon = MaterialDesignThemes.Wpf.PackIconKind.Download, ViewIndex=5, ViewLabel= _titleKey.LoadFromRes<string>(), ViewType=DayZTediratorConstants.ViewTypes.VanillaTool };
             InitializeComponent();
+            DataContext = this;
         }
     }
 }
