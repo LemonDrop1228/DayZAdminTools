e85a0c77b7cc9739b2bbb0cb7f4a00377486b052
gopherskid <gopherskid@gmail.com>
2021-11-02 13:14:05 -0400

    Started work on SpawnPointEditor view
    Started building view

---
+++ Views/SpawnPointsEditor/SpawnPointsEditorView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace DayZTediratorToolz.Views
+{
+    public partial class SpawnPointsEditorView : BaseView
+    {
+        public override ViewMenuData ViewMenuData { get; set; }
+        public SpawnPointsEditorView()
+        {
+            ViewMenuData = new ViewMenuData() { ViewIcon = MaterialDesignThemes.Wpf.PackIconKind.Download, ViewIndex=4   };
+            InitializeComponent();
+        }
+    }
+}
