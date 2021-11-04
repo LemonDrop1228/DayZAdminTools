0874674f6bc3ca5a8442857f8a75ccacc7e89332
LemonDrop1228 <63515535+LemonDrop1228@users.noreply.github.com>
2021-11-04 13:28:53 -0600

    fixed? VS issues

--- Views/BlankView/BlankView.xaml.cs
+++ Views/BlankView/BlankView.xaml.cs
@@ -1,11 +1,23 @@
 ﻿using System.Windows.Controls;
+using DayZTediratorToolz.Helpers;
+using MaterialDesignThemes.Wpf;
 
 namespace DayZTediratorToolz.Views
 {
     public partial class BlankView : BaseView
     {
+        public override ViewMenuData ViewMenuData { get; set; }
+
         public BlankView()
         {
+            ViewMenuData = new ViewMenuData
+            {
+                ViewIndex = 0,
+                ViewLabel = "Test",
+                ViewIcon = PackIconKind.Abacus,
+                ViewType = DayZTediratorConstants.ViewTypes.Info
+            };
+
             InitializeComponent();
         }
     }
